1c0f8e5ff9d00f14a7c5bc5b78649dca3760421d
jhk1090 <jhk1090.public@gmail.com>
2022-07-10 21:20:03 +0900

    Bracket testing

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { title } from "process";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
@@ -86,6 +87,116 @@ export function activate(context: vscode.ExtensionContext) {
       });
     }
   });
+  const toc = vscode.commands.registerCommand("namucode.refresh", async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const document = editor.document;
+      const titleRegex = /^={2,6} .* ={2,6}$/gm;
+      const titles = [...document.getText().matchAll(titleRegex)];
+      const titleLevelCountRegex = /^={2,6}/gm;
+      const titleTextRegex = /(?<=^\={2,6} ).[^=]*(?=\={2,6}$)/gm;
+
+      let dataObject: TreeItem[] = [];
+      function bstr(labelname: string): TreeItem {
+        return {
+          label: labelname,
+          children: [],
+        };
+      }
+      let step1 = -1;
+      let step2 = -1;
+      let step3 = -1;
+      let step4 = -1;
+      for (let i = 0; i < titles.length; i++) {
+        const original = titles[i][0];
+        const level = (
+          original.match(titleLevelCountRegex) as RegExpMatchArray
+        )[0].length;
+        const name = (original.match(titleTextRegex) as RegExpMatchArray)[0];
+        step1 = dataObject.length - 1;
+        try {
+          switch (level) {
+            case 2:
+              dataObject.push(bstr(name));
+              break;
+            case 3:
+              if ("children" in dataObject[step1]) {
+                dataObject[step1].children?.push(bstr(name));
+                step2 = (dataObject[step1].children?.length as number) - 1;
+              }
+              break;
+            case 4:
+              const propertyp4 = (dataObject[step1].children as TreeItem[])[step2]
+              if ("children" in propertyp4) {
+                propertyp4.children?.push(bstr(name));
+                step3 = (propertyp4.children?.length as number) - 1;
+              }
+              break;
+            case 5:
+              const propertyp5 = ((dataObject[step1].children as TreeItem[])[step2].children as TreeItem[])[step3]
+              if ("children" in propertyp5) {
+                propertyp5.children?.push(bstr(name));
+                step4 = (propertyp5.children?.length as number) - 1;
+              }
+              break;
+            case 6:
+              const propertyp6 = (((dataObject[step1].children as TreeItem[])[step2].children as TreeItem[])[step3].children as TreeItem[])[step4]
+              if ("children" in propertyp6) {
+                propertyp6.children?.push(bstr(name));
+              }
+              break;
+            default:
+              break;
+          }
+        } catch (error) {
+            vscode.window.showErrorMessage('문단 조직이 잘못되어 목차가 표시되지 않았습니다. (최상위 문단은 2단계 문단입니다.)');
+            return;
+        }
+      }
+
+      vscode.window.registerTreeDataProvider(
+        "tableOfContent",
+        new OutlineProvider(dataObject)
+      );
+    }
+  });
+
+  class OutlineProvider implements vscode.TreeDataProvider<TreeItem> {
+    constructor(private outline: any) {
+      console.log(outline);
+    }
+
+    getTreeItem(item: any): vscode.TreeItem {
+      return new vscode.TreeItem(
+        item.label,
+        item.children.length > 0
+          ? vscode.TreeItemCollapsibleState.Expanded
+          : vscode.TreeItemCollapsibleState.None
+      );
+    }
+
+    getChildren(element?: any): Thenable<[]> {
+      if (element) {
+        return Promise.resolve(element.children);
+      } else {
+        return Promise.resolve(this.outline);
+      }
+    }
+  }
+
+  class TreeItem extends vscode.TreeItem {
+    children: TreeItem[] | undefined;
+
+    constructor(label: string, children?: TreeItem[]) {
+      super(
+        label,
+        children === undefined
+          ? vscode.TreeItemCollapsibleState.None
+          : vscode.TreeItemCollapsibleState.Expanded
+      );
+      this.children = children;
+    }
+  }
 }
 
 export function deactivate() {}
