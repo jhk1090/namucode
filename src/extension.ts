519ff531d6da5e41a9d378a5a7aabd475c4b44d6
jhk1090 <jhk1090.public@gmail.com>
2023-02-12 14:42:49 +0900

    filesystem removed

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as path from 'path';
-import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
   modifyParagraph();
