2b3a97dfd5f5104ff32b1d75bae8f3abd3a674ee
jhk1090 <jhk1090.public@gmail.com>
2025-10-19 01:05:30 +0900

    frontend 분류 추가

---
+++ client/media/frontend/src/components/wiki/wikiCategory.vue
@@ -0,0 +1,65 @@
+<template>
+  <div ref="el" class="category" :class="{ 'category-folded': isFold }">
+    <span>분류</span>
+    <ul>
+      <template v-for="c in categories">
+        <li :class="{ blur: c.blur }">
+          <a :class="{ 'not-exist': c.notExist }" :href="doc_action_link(c.document, 'w')">{{c.document}}</a>
+        </li>
+      </template>
+    </ul>
+    <div v-if="showCurtain" class="curtain">
+      <GeneralButton class="curtain-button" :whenClick="onClickUnfoldButton">더 보기</GeneralButton>
+    </div>
+  </div>
+</template>
+<script>
+import GeneralButton from '@/components/generalButton'
+
+import Common from '@/mixins/common'
+
+export default {
+  mixins: [Common],
+  components: {
+    GeneralButton
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isFold: true,
+      showCurtain: false
+    }
+  },
+  methods: {
+    onClickUnfoldButton() {
+      this.isFold = false
+      this.showCurtain = false
+    },
+    async recalculate() {
+      this.isFold = true
+      this.showCurtain = false
+      await this.$nextTick()
+      const el = this.$refs.el
+      if(!el) return
+
+      this.showCurtain = el.scrollHeight > el.clientHeight
+    }
+  },
+  mounted() {
+    this.recalculate()
+  },
+  watch: {
+    categories() {
+      this.recalculate()
+    }
+  }
+}
+</script>
+<style scoped>
+@import "@/assets/css/wikiCategory.css"
+</style>
